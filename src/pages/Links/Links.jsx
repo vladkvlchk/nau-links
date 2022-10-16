46391f0dec89b8226340a13590e625322ef2e335
Vladyslav <prostoandrei9@gmail.com>
2022-10-16 12:46:05 +0300

    add select in links

--- src/pages/Links/Links.jsx
+++ src/pages/Links/Links.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+
 import "./Links.module.scss";
+import data from "../../data.json";
 
 const Links = () => {
   let { folder } = useParams();
-  return <div>{folder}</div>;
+  const currentFolder = data.find((obj) => obj.folder === folder).in;
+
+  const [currentFaculty, setCurrentFaculty] = React.useState(
+    currentFolder[0].faculty
+  );
+
+  const handleChange = (event) => {
+
+  }
+
+  return (
+    <>
+      <div>{folder}</div>
+      <select name="faculty" onChange={handleChange}>
+        {currentFolder.map((obj) => {
+          return (<option value={obj.faculty} selected={currentFaculty === obj.faculty}>
+            {obj.faculty}
+          </option>)
+        })}
+      </select>
+    </>
+  );
 };
 
 export default Links;
